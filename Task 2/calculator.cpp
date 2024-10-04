e3b24e43e4c9978b6e544a8cd17ead8454babab9
Deepak32ch <deepakchand083@gmail.com>
2024-10-04 14:27:55 +0530

    Update calculator.cpp

--- Task 2/calculator.cpp
+++ Task 2/calculator.cpp
@@ -1,1 +1,55 @@
+/*This code is for a calculator that perform a basic arithmethic operations*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declare variables
+    double num1, num2, result;
+    char operation;
+
+    // Ask the user to input two numbers
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    // Ask the user to choose an operation
+    cout << "Enter operation (+, -, *, /): ";
+    cin >> operation;
+
+    // Perform the calculation based on the operation entered
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            cout << "Result: " << num1 << " + " << num2 << " = " << result << endl;
+            break;
+
+        case '-':
+            result = num1 - num2;
+            cout << "Result: " << num1 << " - " << num2 << " = " << result << endl;
+            break;
+
+        case '*':
+            result = num1 * num2;
+            cout << "Result: " << num1 << " * " << num2 << " = " << result << endl;
+            break;
+
+        case '/':
+            // Check if the second number is zero to avoid division by zero
+            if (num2 != 0) {
+                result = num1 / num2;
+                cout << "Result: " << num1 << " / " << num2 << " = " << result << endl;
+            } else {
+                cout << "Error! Division by zero is not allowed." << endl;
+            }
+            break;
+
+        default:
+            cout << "Error! Invalid operation." << endl;
+    }
+
+    return 0;
+}
 
